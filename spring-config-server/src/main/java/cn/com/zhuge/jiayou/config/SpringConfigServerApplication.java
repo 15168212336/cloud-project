bef58c3934fcce59cea665ed7f461cbcde3a9b35
soyeaCommnet <yanweimiao@126.com>
2022-02-10 16:50:54 +0800

    Default Changelist

--- spring-config-server/src/main/java/cn/com/zhuge/jiayou/config/SpringConfigServerApplication.java
+++ spring-config-server/src/main/java/cn/com/zhuge/jiayou/config/SpringConfigServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class SpringConfigServerApplication {
 
     public static void main(String[] args) {
