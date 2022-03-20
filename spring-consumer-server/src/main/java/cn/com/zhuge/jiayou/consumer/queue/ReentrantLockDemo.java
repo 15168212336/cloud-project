906b665e765b2269c056d43986065805a22fa0bc
wzf520zwy <695466133@qq.com>
2022-03-20 14:28:25 +0800

    可重入锁

---
+++ spring-consumer-server/src/main/java/cn/com/zhuge/jiayou/consumer/queue/ReentrantLockDemo.java
@@ -0,0 +1,44 @@
+package cn.com.zhuge.jiayou.consumer.queue;
+
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLockCheck reentrantLock= new ReentrantLockCheck();
+        new Thread(reentrantLock, "AA").start();
+//        TimeUnit.SECONDS.sleep(1L);
+        new Thread(reentrantLock, "BB").start();
+
+    }
+}
+
+class ReentrantLockCheck implements Runnable{
+
+    @Override
+    public void run() {
+        if (Thread.currentThread().getName().equals("AA")) {
+            method01();
+        }else {
+            method02();
+        }
+    }
+
+    public synchronized void method01() {
+        try {
+            System.out.println(Thread.currentThread().getName()+"进入方法01");
+            TimeUnit.SECONDS.sleep(2L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        method02();
+    }
+    public synchronized void method02() {
+
+            System.out.println(Thread.currentThread().getName()+"进入方法02");
+
+
+    }
+}
